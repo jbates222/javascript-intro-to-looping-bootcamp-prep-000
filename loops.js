243345276bf32ddbc6642690ac640d778ca95ef9
Jackson Bates <jkbates222@gmail.com>
2018-09-19 01:43:55 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+var arr = [];
+function forLoop(arr) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 0) {
+      arr.push('I am 0 strange loops.');
+    }
+    if (i == 1) {
+      arr.push("I am 1 strange loop.");
+    }
+    else {
+      arr = [...arr, 'I am ${i} strange loops.'];
+    }
+  }
+  return arr;
+}
