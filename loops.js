e9acff684338baa9a3de8683e613eb33d9e67ed8
Jackson Bates <jkbates222@gmail.com>
2018-09-19 01:48:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,8 @@
 var arr = [];
 function forLoop(arr) {
   for (let i = 0; i < 25; i++) {
-    if (i === 0) {
-      arr.push('I am 0 strange loops.');
-    }
     if (i == 1) {
-      arr = [...arr, "I am 1 strange loop."];
+      arr.push("I am 1 strange loop.");
       i++;
     }
     else {
